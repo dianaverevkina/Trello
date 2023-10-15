8e50f91c53c440e4ba4c0616abc4181633a315f9
Diana Verevkina <diana_verevkina@mail.ru>
2023-10-15 19:36:51 +0300

    added opportunity to move task

--- src/js/Widget.js
+++ src/js/Widget.js
@@ -1,5 +1,5 @@
-import tasks from "./tasks";
-import Task from "./Task";
+import tasks from './tasks';
+import Task from './Task';
 
 export default class Widget {
   constructor(app, state) {
@@ -9,27 +9,135 @@ export default class Widget {
     this.listsBtns = app.listsBtns;
 
     this.showInputForAddingTask = this.showInputForAddingTask.bind(this);
+    this.onMouseDown = this.onMouseDown.bind(this);
+    this.onMouseMove = this.onMouseMove.bind(this);
+    this.onMouseUp = this.onMouseUp.bind(this);
 
     this.addEvents();
   }
 
   addEvents() {
-    this.lists.forEach(list => {
-      list.addEventListener('mousedown', () => console.log('here'));
-    });
+    this.lists.forEach((list) => list.addEventListener('mousedown', this.onMouseDown));
+    this.listsBtns.forEach((btn) => btn.addEventListener('click', this.showInputForAddingTask));
+  }
 
-    this.listsBtns.forEach(btn => btn.addEventListener('click', this.showInputForAddingTask));
+  addEventsFortask(task) {
+    const btnDeleteTask = task.querySelector('.task__delete');
+    task.addEventListener('mouseover', () => btnDeleteTask.classList.add('task__delete_visible'));
+    task.addEventListener('mouseout', () => btnDeleteTask.classList.remove('task__delete_visible'));
+  }
+
+  // Удаляем задачу при клике по кнопке "Крестик"
+  deleteTask(btn) {
+    const currentTask = btn.closest('.task');
+    const list = currentTask.closest('.list');
+    const listItems = list.querySelector('.list__items');
+    const nameList = list.getAttribute('data-name');
+    const taskIndex = tasks.findIndex(item => item.id === +currentTask.getAttribute('data-id'));
+    tasks.splice(taskIndex, 1);
+    this.state.save(tasks);
+    this.renewList(listItems, nameList);
+  }
+
+  createPlaceHolder() {
+    this.placeForTask = null;
+    this.placeForTask = document.createElement('li');
+    this.placeForTask.classList.add('list__placeholder', 'task-placeholder');
+    this.placeForTask.style.height = `${this.draggedTask.offsetHeight}px`;
+  }
+
+  moveTask(e) {
+    this.draggedTask.style.top = `${e.clientY - this.dragOffsetY}px`;
+    this.draggedTask.style.left = `${e.clientX - this.dragOffsetX}px`;
+  }
+
+  onMouseDown(e) {
+    e.preventDefault();
+    const btnDelete = e.target.closest('.task__delete');
+    if (btnDelete) {
+      this.deleteTask(btnDelete);
+      return;
+    }
+
+    this.draggedTask = e.target.closest('.task');
+    if (!this.draggedTask) return;
+
+    this.createPlaceHolder();
+
+    this.dragOffsetX = e.clientX - this.draggedTask.getBoundingClientRect().left;
+    this.dragOffsetY = e.clientY - this.draggedTask.getBoundingClientRect().top;
+
+    this.moveTask(e);
+
+    this.draggedTask.classList.add('task_dragged');
+
+    document.documentElement.addEventListener('mouseup', this.onMouseUp);
+    document.documentElement.addEventListener('mousemove', this.onMouseMove);
+  }
+
+  onMouseMove(e) {
+    e.preventDefault();
+    
+    this.moveTask(e);
+    this.showPlaceForTask(e);
+  }
+
+  showPlaceForTask(event) {
+    const task = event.target.closest('.task');
+    const list = event.target.closest('.list');
+  
+    if (!list) return;
+
+    const container = list.querySelector('.list__items');
+    const items = list.querySelectorAll('.task');
+
+    if (container && items.length === 0) {
+      container.append(this.placeForTask);
+      return;
+    }
+
+    if (!task) return;
+    const { top, height } = task.getBoundingClientRect();
+    const taskCenterY = top + height / 2;
+
+    if (event.clientY < taskCenterY) {
+      task.before(this.placeForTask);
+    } else {
+      task.after(this.placeForTask);
+    }
+  }
+
+  onMouseUp() {
+    this.placeForTask.replaceWith(this.draggedTask);
+
+    this.draggedTask.classList.remove('task_dragged');
+    this.draggedTask.style.top = 0;
+    this.draggedTask.style.left = 0;
+
+    const list = this.draggedTask.closest('.list');
+    const listName = list.getAttribute('data-name');
+    const foundTask = tasks.find((task) => task.id === +this.draggedTask.getAttribute('data-id'));
+    foundTask.state = listName;
+    this.state.save(tasks);
+
+    this.placeForTask.remove();
+    this.draggedTask = null;
+
+    document.documentElement.removeEventListener('mouseup', this.onMouseUp);
+    document.documentElement.removeEventListener('mousemove', this.onMouseMove);
   }
 
   showInputForAddingTask(e) {
     e.preventDefault();
-    const { target } = e;
-    const list = target.closest('.list');
-    this.listItems = list.querySelector('.list__items');
-    this.listName = this.listItems.getAttribute('data-name');
+    const { currentTarget } = e;
+
+    if (this.blockForTask) {
+      this.blockForTask.remove();
+    }
+    
     this.blockForTask = this.app.drawFieldForTask();
 
-    this.listItems.append(this.blockForTask);
+    currentTarget.before(this.blockForTask);
     this.fieldForTask = this.blockForTask.querySelector('.input__add-task');
     const btnAddTask = this.blockForTask.querySelector('.task__btn-add');
     const btnCancelTask = this.blockForTask.querySelector('.task__btn-cancel');
@@ -40,24 +148,29 @@ export default class Widget {
 
   addTask(e) {
     e.preventDefault();
-    const { target } = e;
+    if (!this.fieldForTask.value.trim()) return;
+
+    const list = e.target.closest('.list');
+    const listItems = list.querySelector('.list__items');
+    const listName = listItems.getAttribute('data-name');
     const id = Math.random() * 100000;
-    const task = new Task(this.fieldForTask.value, id, this.listName);
+
+    const task = new Task(this.fieldForTask.value, id, listName);
     tasks.push(task);
+    this.state.save(tasks);
     this.blockForTask.remove();
-    this.renewList();
+    this.renewList(listItems, listName);
   }
 
-  renewList() {
-    const items = this.listItems.querySelectorAll('.task');
-    items.forEach(item => item.remove());
-    const filteredTasks = tasks.filter(task => task.state === this.listName);
-    filteredTasks.forEach(task => {
-      const el = this.app.drawTaskElement(task.text);
-      this.listItems.append(el);
+  renewList(list, listName) {
+    const items = list.querySelectorAll('.task');
+    items.forEach((item) => item.remove());
+    const filteredTasks = tasks.filter((task) => task.state === listName);
+    filteredTasks.forEach((task) => {
+      const el = this.app.drawTaskElement(task.text, task.id);
+      list.append(el);
+      this.addEventsFortask(el);
     });
-
-    this.state.save(tasks);
   }
 
   cancelTask(e) {
@@ -67,12 +180,14 @@ export default class Widget {
 
   loadTasks() {
     const savedTasks = this.state.load();
-
     if (savedTasks) {
-      savedTasks.forEach(task => {
-        const taskEl = this.app.drawTaskElement(task.text);
+      savedTasks.forEach(obj => tasks.push(obj));
+      tasks.forEach((task) => {
+        const taskEl = this.app.drawTaskElement(task.text, task.id);
         const list = this.app.container.querySelector(`[data-name=${task.state}]`);
-        list.append(taskEl);
+        list.querySelector('.list__items').append(taskEl);
+
+        this.addEventsFortask(taskEl);
       });
     }
   }
